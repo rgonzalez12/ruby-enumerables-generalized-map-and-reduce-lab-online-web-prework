4d75b3317dbd1a709bcedb2088c7be9f5749b108
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-10 02:43:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 # Your Code Here
-def map(array)
+def map(source_array)
   new = []
   i = 0
-  while i < array.length
-  new.push(yield(array[i]))
+  while i < source_array.length
+  new.push(yield(source_array[i]))
   i += 1
   end
   new
