c8fe9a692fa842f1ed0c0dfc8abe11e206400867
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-10 02:29:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def my_own_map(source_array, block)
+   map([source_array]) {|n| n * -1} 
+   map([source_array]) {|n| n + 1}
+   map([source_array]) {|n| n * 2}
+   map([source_array]) {|n| n ** 2}
+end
+
+def my_own_reduce(source_array, starting_pont = 0)
+  {|sum, num| sum + num}
+end
+  
