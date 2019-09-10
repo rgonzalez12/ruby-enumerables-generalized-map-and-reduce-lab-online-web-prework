98719c153b463e6719ef069c87c3dbfc33abe435
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-10 02:35:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 # Your Code Here
-def my_own_map(source_array, block)
+def map(source_array, block)
   
 end
 
-def my_own_reduce(source_array, starting_pont = 0)
+def reduce(source_array, starting_point = 0)
   
 end
   
